3dfbfb33fbdf59e85bfde878a10de0f007039a16
sant <sganis@gmail.com>
2018-12-30 18:39:50 +0300

    Improved build.py

--- tool/build.py
+++ tool/build.py
@@ -5,26 +5,23 @@ import sys
 import os
 import time
 
-BUILD_DIR = 'build'
 DIR = os.path.dirname(os.path.realpath(__file__))
-
-def build():	
+	
+def build(BUILD_DIR):	
 	t = time.time()
 	print('Building release mode...')
-
-	assert not os.path.exists(BUILD_DIR)
+	
+	SRC = os.path.realpath(f'{DIR}/..')
 
 	if os.name == 'nt':
-		os.system(f'if exist "{BUILD_DIR}" rd /s /q "{BUILD_DIR}"')
 		os.system(fr'md "{BUILD_DIR}\Release"')
 		os.chdir(f'{BUILD_DIR}')
-		os.system('cmake ../.. -G "Visual Studio 14" -A x64')
+		os.system(f'cmake {SRC} -G "Visual Studio 14" -A x64')
 		# os.system('msbuild /P:Configuration=Release ALL_BUILD.vcxproj')
 		os.system('cmake --build . --target ALL_BUILD --config Release --parallel 8')
 	else:
-		os.system(f'rm -rf {BUILD_DIR} && mkdir -p {BUILD_DIR}')
 		os.chdir(BUILD_DIR)
-		os.system('cmake ../.. && make -j8')
+		os.system(f'cmake {SRC} && make -j8')
 
 	print('\nDone.')
 	print('Binaries    : {}'.format(os.path.realpath('.')))
@@ -34,7 +31,10 @@ def build():
 
 
 if __name__ == '__main__':	
-	build()
+
+	assert len(sys.argv) > 1 # usage: ./build.py <build_dir>
+	BUILD_DIR = os.path.realpath(sys.argv[1])
+	build(BUILD_DIR)
 
 		
 	
